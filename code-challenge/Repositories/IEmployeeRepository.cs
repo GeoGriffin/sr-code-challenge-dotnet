c5a8ddc1d77720f398af1d15571e3271efca81ed
kurt.devlin <kurt.devlin@tr.com>
2018-11-24 19:30:04 -0500

    Add project files.

---
+++ code-challenge/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using challenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace challenge.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetById(String id);
+        List<Employee> GetAll();
+        Employee Add(Employee employee);
+        Employee Remove(Employee employee);
+        Task SaveAsync();
+    }
+}
